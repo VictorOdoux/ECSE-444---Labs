1583605e0d8913c42460012953c6cab51a63afb9
Victor Odoux <victor.odoux@mail.mcgill.ca>
2026-03-14 17:42:20 -0400

    LAB 4 + Part 1 completed

---
+++ LAB 4/Core/Src/main.c
@@ -0,0 +1,459 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "stm32l4s5i_iot01_hsensor.h"
+#include "stm32l4s5i_iot01_magneto.h"
+#include "stm32l4s5i_iot01_accelero.h"
+#include "stm32l4s5i_iot01_psensor.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef enum
+{
+  DISPLAY_HUMIDITY = 0,
+  DISPLAY_MAGNETO,
+  DISPLAY_ACCELERO,
+  DISPLAY_PRESSURE,
+  DISPLAY_COUNT
+} DisplayMode_t;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* Blue user button B2 on the B-L4S5I-IOT01A board */
+#define USER_BUTTON_PORT GPIOC
+#define USER_BUTTON_PIN  GPIO_PIN_13
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c2;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+static char uartBuf[128];
+
+static DisplayMode_t displayMode = DISPLAY_HUMIDITY;
+
+/* One selected quantity from each sensor */
+static float humidity_pct = 0.0f;     /* HTS221 */
+static int16_t magXYZ[3] = {0};       /* LIS3MDL */
+static int16_t accXYZ[3] = {0};       /* LSM6DSL */
+static float pressure_hPa = 0.0f;     /* LPS22HB */
+
+static GPIO_PinState lastButtonState = GPIO_PIN_SET;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_USART1_UART_Init(void);
+/* USER CODE BEGIN PFP */
+static void Sensors_Init(void);
+static void Sensors_ReadAll(void);
+static void Handle_Button(void);
+static void UART_SendSelectedSensor(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+static void Sensors_Init(void)
+{
+  if (BSP_HSENSOR_Init() != 0U)
+  {
+    Error_Handler();
+  }
+
+  if (BSP_MAGNETO_Init() != MAGNETO_OK)
+  {
+    Error_Handler();
+  }
+
+  if (BSP_ACCELERO_Init() != ACCELERO_OK)
+  {
+    Error_Handler();
+  }
+
+  if (BSP_PSENSOR_Init() != 0U)
+  {
+    Error_Handler();
+  }
+}
+
+static void Sensors_ReadAll(void)
+{
+  /* HTS221: humidity */
+  humidity_pct = BSP_HSENSOR_ReadHumidity();
+
+  /* LIS3MDL: magnetometer X/Y/Z */
+  BSP_MAGNETO_GetXYZ(magXYZ);
+
+  /* LSM6DSL: accelerometer X/Y/Z */
+  BSP_ACCELERO_AccGetXYZ(accXYZ);
+
+  /* LPS22HB: pressure */
+  pressure_hPa = BSP_PSENSOR_ReadPressure();
+}
+
+static void Handle_Button(void)
+{
+  GPIO_PinState currentButtonState = HAL_GPIO_ReadPin(USER_BUTTON_PORT, USER_BUTTON_PIN);
+
+  /* B2 is active-low: detect one falling edge per press */
+  if ((lastButtonState == GPIO_PIN_SET) && (currentButtonState == GPIO_PIN_RESET))
+  {
+    displayMode = (DisplayMode_t)((displayMode + 1) % DISPLAY_COUNT);
+
+    /* simple debounce */
+    HAL_Delay(20);
+    while (HAL_GPIO_ReadPin(USER_BUTTON_PORT, USER_BUTTON_PIN) == GPIO_PIN_RESET)
+    {
+      HAL_Delay(5);
+    }
+  }
+
+  lastButtonState = HAL_GPIO_ReadPin(USER_BUTTON_PORT, USER_BUTTON_PIN);
+}
+
+static void UART_SendSelectedSensor(void)
+{
+  int len = 0;
+
+  /* Cast floats to int for easy UART printing in Part 1 */
+  switch (displayMode)
+  {
+    case DISPLAY_HUMIDITY:
+      len = snprintf(uartBuf, sizeof(uartBuf),
+                     "HTS221 Humidity = %d %%RH\r\n",
+                     (int)humidity_pct);
+      break;
+
+    case DISPLAY_MAGNETO:
+      len = snprintf(uartBuf, sizeof(uartBuf),
+                     "LIS3MDL Mag XYZ = %d, %d, %d\r\n",
+                     magXYZ[0], magXYZ[1], magXYZ[2]);
+      break;
+
+    case DISPLAY_ACCELERO:
+      len = snprintf(uartBuf, sizeof(uartBuf),
+                     "LSM6DSL Acc XYZ = %d, %d, %d\r\n",
+                     accXYZ[0], accXYZ[1], accXYZ[2]);
+      break;
+
+    case DISPLAY_PRESSURE:
+      len = snprintf(uartBuf, sizeof(uartBuf),
+                     "LPS22HB Pressure = %d hPa\r\n",
+                     (int)pressure_hPa);
+      break;
+
+    default:
+      len = snprintf(uartBuf, sizeof(uartBuf), "Invalid display mode\r\n");
+      break;
+  }
+
+  if (len > 0)
+  {
+    HAL_UART_Transmit(&huart1, (uint8_t *)uartBuf, (uint16_t)len, 100);
+  }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C2_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+  const char *msg = "UART alive\r\n";
+  HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), 100);
+  Sensors_Init();
+
+   const char *startupMsg =
+       "\r\nLab 4 Part 1 started. Press B2 to cycle sensors.\r\n";
+   HAL_UART_Transmit(&huart1, (uint8_t *)startupMsg, (uint16_t)strlen(startupMsg), 100);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  Sensors_ReadAll();
+	  UART_SendSelectedSensor();
+
+	      for (int i = 0; i < 10; i++)
+	      {
+	          Handle_Button();
+	          HAL_Delay(10);
+	      }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 60;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x30A175AB;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ LAB 4/Debug/Core/Src/main.d
@@ -0,0 +1,95 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ ../Core/Inc/stm32l4xx_hal_conf.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4s5xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_dma.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_dmamux.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ ../Core/Inc/stm32l4s5i_iot01_hsensor.h ../Core/Inc/stm32l4s5i_iot01.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/hts221/hts221.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/hts221/../Common/hsensor.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/hts221/../Common/tsensor.h \
+ ../Core/Inc/stm32l4s5i_iot01_magneto.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lis3mdl/lis3mdl.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lis3mdl/../Common/magneto.h \
+ ../Core/Inc/stm32l4s5i_iot01_accelero.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lsm6dsl/lsm6dsl.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lsm6dsl/../Common/accelero.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lsm6dsl/../Common/gyro.h \
+ ../Core/Inc/stm32l4s5i_iot01_psensor.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lps22hb/lps22hb.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lps22hb/../Common/psensor.h \
+ C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lps22hb/../Common/tsensor.h
+../Core/Inc/main.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+../Core/Inc/stm32l4xx_hal_conf.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4s5xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_dma.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_dmamux.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
+../Core/Inc/stm32l4s5i_iot01_hsensor.h:
+../Core/Inc/stm32l4s5i_iot01.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/hts221/hts221.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/hts221/../Common/hsensor.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/hts221/../Common/tsensor.h:
+../Core/Inc/stm32l4s5i_iot01_magneto.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lis3mdl/lis3mdl.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lis3mdl/../Common/magneto.h:
+../Core/Inc/stm32l4s5i_iot01_accelero.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lsm6dsl/lsm6dsl.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lsm6dsl/../Common/accelero.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lsm6dsl/../Common/gyro.h:
+../Core/Inc/stm32l4s5i_iot01_psensor.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lps22hb/lps22hb.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lps22hb/../Common/psensor.h:
+C:/Users/victo/Documents/ECSE\ 321/ECSE-444---Labs/LAB\ 4/Drivers/Components/../Components/lps22hb/../Common/tsensor.h:
